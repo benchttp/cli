af98e2bf7574722091e404390e8012f8a45aa45b
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-11-05 13:05:03 +0100

    test(configflag): refactor tests
    - use benchttptest.AssertEqualRunners

--- internal/configflag/bind_test.go
+++ internal/configflag/bind_test.go
@@ -1,37 +1,40 @@
 package configflag_test
 
 import (
+	"bytes"
 	"flag"
-	"reflect"
+	"io"
+	"net/http"
+	"net/url"
 	"testing"
+	"time"
 
+	"github.com/benchttp/sdk/benchttp"
+	"github.com/benchttp/sdk/benchttptest"
 	"github.com/benchttp/sdk/configio"
 
 	"github.com/benchttp/cli/internal/configflag"
 )
 
 func TestBind(t *testing.T) {
-	t.Run("default to zero representation", func(t *testing.T) {
+	t.Run("default to zero runner", func(t *testing.T) {
 		flagset := flag.NewFlagSet("", flag.ExitOnError)
 		args := []string{} // no args
 
-		repr := configio.Representation{}
-		configflag.Bind(flagset, &repr)
+		b := configio.Builder{}
+		configflag.Bind(flagset, &b)
 		if err := flagset.Parse(args); err != nil {
 			t.Fatal(err) // critical error, stop the test
 		}
 
-		exp := configio.Representation{}
-		if got := repr; !reflect.DeepEqual(got, exp) {
-			t.Errorf("\nexp %#v\ngot %#v", exp, got)
-		}
+		benchttptest.AssertEqualRunners(t, benchttp.Runner{}, b.Runner())
 	})
 
 	t.Run("set config with flags values", func(t *testing.T) {
 		flagset := flag.NewFlagSet("", flag.ExitOnError)
 		args := []string{
 			"-method", "POST",
-			"-url", "https://benchttp.app?cool=yes",
+			"-url", "https://example.com?a=b",
 			"-header", "API_KEY:abc",
 			"-header", "Accept:text/html",
 			"-header", "Accept:application/json",
@@ -43,28 +46,38 @@ func TestBind(t *testing.T) {
 			"-globalTimeout", "5s",
 		}
 
-		repr := configio.Representation{}
-		configflag.Bind(flagset, &repr)
+		b := configio.Builder{}
+		configflag.Bind(flagset, &b)
 		if err := flagset.Parse(args); err != nil {
 			t.Fatal(err) // critical error, stop the test
 		}
 
-		switch {
-		case *repr.Request.Method != "POST",
-			*repr.Request.URL != "https://benchttp.app?cool=yes",
-			repr.Request.Header["API_KEY"][0] != "abc",
-			repr.Request.Header["Accept"][0] != "text/html",
-			repr.Request.Header["Accept"][1] != "application/json",
-			repr.Request.Body.Type != "raw",
-			repr.Request.Body.Content != "hello",
-
-			*repr.Runner.Requests != 1,
-			*repr.Runner.Concurrency != 2,
-			*repr.Runner.Interval != "3s",
-			*repr.Runner.RequestTimeout != "4s",
-			*repr.Runner.GlobalTimeout != "5s":
-
-			t.Error("unexpected parsed flags")
-		}
+		benchttptest.AssertEqualRunners(t,
+			benchttp.Runner{
+				Request: &http.Request{
+					Method: "POST",
+					URL:    mustParseURL("https://example.com?a=b"),
+					Header: http.Header{
+						"API_KEY": []string{"abc"},
+						"Accept":  []string{"text/html", "application/json"},
+					},
+					Body: io.NopCloser(bytes.NewBufferString("hello")),
+				},
+				Requests:       1,
+				Concurrency:    2,
+				Interval:       3 * time.Second,
+				RequestTimeout: 4 * time.Second,
+				GlobalTimeout:  5 * time.Second,
+			},
+			b.Runner(),
+		)
 	})
+}
+
+func mustParseURL(v string) *url.URL {
+	u, err := url.ParseRequestURI(v)
+	if err != nil {
+		panic("mustParseURL: " + err.Error())
+	}
+	return u
 }
